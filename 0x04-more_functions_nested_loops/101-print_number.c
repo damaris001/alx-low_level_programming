a928f6eac0040d4fbb9620b89fea4ee5af678d9d
damaris001 <91469271+damaris001@users.noreply.github.com>
2022-10-23 12:21:22 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,16 +18,16 @@ void print_number(int n)
 		i = n;
 	}
 
-	j = 1;
+	j = i;
 	count = 1;
 
 	while (j > 9)
 	{
-		j/= 10;
+		j /= 10;
 		count *= 10;
 	}
-	
-	for(; count >= 1; count /= 10)
+
+	for (; count >= 1; count /= 10)
 	{
 		_putchar(((i / count) % 10) + 48);
 	}
